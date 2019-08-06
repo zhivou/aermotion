0444ff6453d9130bd8a84548b76f061d9ff96d72
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-08-06 10:32:38 -0600

    Added form, added link to Types, added nav items

--- app/models/workout_set.rb
+++ app/models/workout_set.rb
@@ -1,4 +1,6 @@
 class WorkoutSet < ApplicationRecord
   belongs_to :type
   has_and_belongs_to_many :users
+  has_many_attached :videos
+  accepts_nested_attributes_for :type, allow_destroy: true
 end
