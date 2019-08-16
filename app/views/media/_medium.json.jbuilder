291434c8150c0ecb6c44b693efe7b98e29b0bb96
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-08-16 15:03:07 -0600

    Added Media for Connecting Sets with Videos

---
+++ app/views/media/_medium.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! medium, :id, :title, :created_at, :updated_at
+json.url medium_url(medium, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_022922) do
+ActiveRecord::Schema.define(version: 2019_08_16_205920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "media", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "types", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
