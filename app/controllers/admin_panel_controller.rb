b07e722d93da1984e97920b3c2c52ef5db4670cc
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-08-16 21:49:28 -0600

    Added admin overwrites for video and user addings

--- app/controllers/admin_panel_controller.rb
+++ app/controllers/admin_panel_controller.rb
@@ -2,4 +2,10 @@ class AdminPanelController < ApplicationController
   def index
     @users = User.all
   end
+
+  def add_video
+  end
+
+  def add_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'admin_panel', to: 'admin_panel#index'
+  get 'admin_add_video', to: "admin_panel#add_video"
+  get 'admin_add_user', to: "admin_panel#add_user"
   resources :media
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
