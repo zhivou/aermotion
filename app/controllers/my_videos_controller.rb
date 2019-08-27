40e368cb871ac4bb0d61d6976c265bb02d91f09d
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-08-26 20:09:56 -0600

    Added video container for user videos

--- app/controllers/my_videos_controller.rb
+++ app/controllers/my_videos_controller.rb
@@ -1,18 +1,27 @@
 class MyVideosController < ApplicationController
 
+  before_action :get_media_per_user, only: [:index]
+
   def index
-    container = {}
-    current_user.workout_sets.each do |set|
-      container[set.title] = []
-      set.media.each do |v|
-        container[set.title] << rails_blob_path(v.video)
-      end
-    end
-    gon.mediaSets = container
   end
 
   private
-    def my_video_params
-      params.fetch(:my_video, {})
+  ##
+  # Returns data set for JS injection:
+  # [{ title, type, video_url },{} ... {}]
+  #
+  def get_media_per_user
+    container = []
+    current_user.workout_sets.each do |set|
+      hash = {}
+      hash[:title] = set.title
+      hash[:type] = Type.find(set.type_id).name
+      hash[:video] = []
+      set.media.each do |v|
+        hash[:video] << rails_blob_path(v.video)
+      end
+      container << hash
     end
+    gon.mediaSets = container
+  end
 end
