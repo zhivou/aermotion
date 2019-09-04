60b4f39c862437d934e6bdc3b32630853ca4c934
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-09-04 14:25:39 -0600

    Added PayPalTransaction table, added population step to payment execute method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_185331) do
+ActiveRecord::Schema.define(version: 2019_09_04_195238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 2019_09_04_185331) do
 
   create_table "pay_pal_transactions", force: :cascade do |t|
     t.string "transaction_id"
-    t.string "string"
     t.integer "local_user_id"
     t.string "local_user_email"
     t.string "method"
@@ -66,6 +65,7 @@ ActiveRecord::Schema.define(version: 2019_09_04_185331) do
     t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "error"
   end
 
   create_table "types", force: :cascade do |t|
