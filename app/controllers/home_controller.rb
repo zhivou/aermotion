ac927705f93f0c77cb874b32f7bfd03b7e78ed86
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-09-21 23:36:01 -0600

    Added latest workouts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @workout_sets = WorkoutSet.order("created_at desc").includes(:type).limit(5)
+    @workout_sets = WorkoutSet.order("created_at desc").includes(:type).limit(6)
   end
 end
