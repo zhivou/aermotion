4d6870e85de7bf33324708df544d8c1722785d2b
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-10-04 20:45:00 -0600

    Added workout style fixes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,6 +15,7 @@ Rails.application.config.assets.precompile += %w( my_videos.css )
 Rails.application.config.assets.precompile += %w( blogs.css )
 Rails.application.config.assets.precompile += %w( workout_sets.css )
 Rails.application.config.assets.precompile += %w( hover_grid.css )
+Rails.application.config.assets.precompile += %w( tutorial.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
