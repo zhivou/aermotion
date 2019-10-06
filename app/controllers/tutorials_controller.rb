f81d9df0675f58b3897a4392c956a2dba5b03439
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-10-06 13:31:23 -0600

    Added same logic for tutorials

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,5 +1,9 @@
 class TutorialsController < ApplicationController
   def index
-    @tutorials = WorkoutSet.order("created_at desc").includes(:type).where(type: Type.where(name:"Tutorial").take.id)
+    if user_signed_in?
+      @tutorials = WorkoutSet.includes(:type).where(type: Type.where(name:"Tutorial").take.id).where.not(id: current_user.workout_sets.includes(:type).each {|i| i})
+    else
+      @tutorials = WorkoutSet.order("created_at desc").includes(:type).where(type: Type.where(name:"Tutorial").take.id)
+    end
   end
 end
