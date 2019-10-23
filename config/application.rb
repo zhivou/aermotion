c7e00b8fdf446ddb8f5aeb1aa0920ef10d94cde2
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-10-22 20:34:31 -0600

    Work around for zeitwerk

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Aermotion
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.autoloader = :classic # TODO remove once https://github.com/solidusio/solidus_auth_devise/issues/174 is fixed
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
