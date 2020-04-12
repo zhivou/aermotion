9be3693b0fd51a8a047252196018330bc728ebd7
zhivou <zivou4@gmail.com>
2020-04-12 14:48:24 -0600

    Added page title and dynamic data to home and workouts

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -11,5 +11,7 @@ module DefaultPageContent
   #
   def set_page_defaults
     @site_defaults = SiteConfiguration.all
+    @page_title ||= 'Aeromotion Pages'
+    @seo_keywords ||= 'Aeromotion, Tutorials, Workouts, Best Trainings, Free Videos, Personal, Best'
   end
 end
